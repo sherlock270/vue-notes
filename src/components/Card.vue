00f14cac6df61453d7403acbd51e8952710a0e67
Charles Cortinas <crcortinas@gmail.com>
2019-03-01 00:33:02 -0600

    Display cards in ViewNotes view

---
+++ src/components/Card.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <router-link :to="{ name: 'view-note', params: { id: note.id } }"
+      >Note #{{ note.id }}</router-link
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["note"]
+};
+</script>
+
+<style scoped></style>
--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,9 +1,39 @@
 <template>
-  <div>View all notes</div>
+  <div>
+    <h1>all notes</h1>
+    <Card v-for="note in notes" :key="note.id" :note="note" />
+  </div>
 </template>
 
 <script>
-export default {};
+import Card from "@/components/Card.vue";
+
+export default {
+  components: {
+    Card
+  },
+  data() {
+    return {
+      notes: [
+        {
+          id: 1,
+          title: "title 1",
+          content: "contents of the note"
+        },
+        {
+          id: 2,
+          title: "title 2",
+          content: "contents of the note"
+        },
+        {
+          id: 3,
+          title: "title 3",
+          content: "contents of the note"
+        }
+      ]
+    };
+  }
+};
 </script>
 
 <style scoped></style>
